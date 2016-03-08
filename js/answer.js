fa4eb9c5e017b90ec8d475e7fac84973d892bddc
dpweb <dpweb@G-2.bavc-int.org>
2016-03-07 19:00:36 -0800

    update Oppression project

---
+++ js/answer.js
@@ -0,0 +1,31 @@
+
+
+      var answers = [
+        "it needs to STOP.",
+        "we need to CHANGE for the BETTER."
+      ];
+
+        function fortune() {
+          num = Math.round( (answers.length - 1) * Math.random());
+          return answers[num];
+        }
+
+      var answers3 = [
+        "it needs to STOP.",
+        "we need to CHANGE for the BETTER."
+      ];
+
+        function fortune() {
+          num = Math.round( (answers.length - 1) * Math.random());
+          return3 answers[num];
+        }
+
+      var answers4 = [
+        "Stop.",
+        "Say it with me.",
+      ];
+
+        function fortune() {
+          num = Math.round( (answers.length - 1) * Math.random());
+          return answers4[num];
+        }
